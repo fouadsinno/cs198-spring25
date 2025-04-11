28872c8905e9c719ac0ca33da732571ca61604ed
fouadsinno <fsinno@berkeley.edu>
2025-04-10 22:13:45 -0700

    starting my shopping list app

--- week8/fouad_sinno/index.js
+++ week8/fouad_sinno/index.js
@@ -33,3 +33,7 @@ server.listen(3000, async function(){
   await mongoose.connect("mongodb+srv://fsinno:week7pass@fouadweek7.gwzjg.mongodb.net/?retryWrites=true&w=majority&appName=FouadWeek7")
   console.log('listening on *:3000');
 });
+
+app.get('/messages', async function(req, res){
+  res.json(await messageModel.find());
+});
